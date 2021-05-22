e7feec57e3be859f2aef4fee95eb06a95a207063
todak-sun <tjsdydwn@gmail.com>
2021-05-22 22:00:54 +0900

    init

---
+++ src/main/java/io/todak/study/javateststudy/JavaTestStudyApplication.java
@@ -0,0 +1,13 @@
+package io.todak.study.javateststudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/todak/study/javateststudy/JavaTestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.todak.study.javateststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
