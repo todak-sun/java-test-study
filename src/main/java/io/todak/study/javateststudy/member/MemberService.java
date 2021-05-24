4a4577b5e99d85c6561b0647513faaed0cb5ced9
todak-sun <tjsdydwn@gmail.com>
2021-05-24 23:45:07 +0900

    MemberService 추가

--- src/main/java/io/todak/study/javateststudy/member/MemberService.java
+++ src/main/java/io/todak/study/javateststudy/member/MemberService.java
@@ -1,9 +1,17 @@
 package io.todak.study.javateststudy.member;
 
 import io.todak.study.javateststudy.domain.Member;
+import io.todak.study.javateststudy.domain.Study;
 
 import java.util.Optional;
 
 public interface MemberService {
     Optional<Member> findById(Long memberId) throws MemberNotFoundException;
+
+    void validate(Long memberId);
+
+    void notify(Study newStudy);
+
+    void notify(Member member);
+
 }
--- src/test/java/io/todak/study/javateststudy/study/StudyServiceTest.java
+++ src/test/java/io/todak/study/javateststudy/study/StudyServiceTest.java
@@ -3,6 +3,8 @@ package io.todak.study.javateststudy.study;
 import io.todak.study.javateststudy.domain.Member;
 import io.todak.study.javateststudy.domain.Study;
 import io.todak.study.javateststudy.member.MemberService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -10,18 +12,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
-
-//    @Mock
-//    MemberService memberService;
-//    @Mock
-//    StudyRepository studyRepository;
 
     StudyService studyService;
 
@@ -32,11 +28,12 @@ class StudyServiceTest {
         assertNotNull(studyService);
     }
 
+    @DisplayName("Stubbing 해보기")
     @Test
     public void test2() {
-
         MemberService memberService = mock(MemberService.class);
         StudyRepository studyRepository = mock(StudyRepository.class);
+
         StudyService studyService = new StudyService(memberService, studyRepository);
 
         // stubbing
@@ -46,7 +43,6 @@ class StudyServiceTest {
         when(memberService.findById(1L)).thenReturn(Optional.of(member));
 
         Study study = new Study(10, "java");
-
     }
 
 }
